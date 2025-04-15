5dce8a75a77d203562a3114c400ad70a54fb21db
Linh <truongthinhatlinh2408@gmail.com>
2025-04-15 23:41:59 +0700

    Upload website

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const observer = new IntersectionObserver((entries, observer) => {
 document.querySelectorAll('.fade-in').forEach(el => observer.observe(el));
 
 // JavaScript để kích hoạt trang kéo khi nhấn vào kính lúp
-document.getElementById('hamburger-icon').addEventListener('click', function() {
+document.getElementById('search-icon').addEventListener('click', function() {
     document.getElementById('side-panel').classList.toggle('open');
 });
 // Lấy tất cả các nút mũi tên
@@ -57,53 +57,4 @@ toggleBtns.forEach(button => {
         this.classList.toggle('active');
     });
 });
-// Slider: Thêm nút chuyển tiếp (trái và phải)
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-const eventsContainer = document.querySelector('.events-container');
-let currentIndex = 0;
-
-function moveSlider(direction) {
-    const eventItems = document.querySelectorAll('.event-item');
-    const totalItems = eventItems.length;
-
-    if (direction === 'prev') {
-        currentIndex = (currentIndex === 0) ? totalItems - 1 : currentIndex - 1;
-    } else {
-        currentIndex = (currentIndex === totalItems - 1) ? 0 : currentIndex + 1;
-    }
-
-    eventsContainer.style.transform = `translateX(-${currentIndex * 25}%)`;
-}
-
-// Tạo slider cho phần Explore Courses
-const explorePrevButton = document.querySelector('.explore-prev');
-const exploreNextButton = document.querySelector('.explore-next');
-const exploreCoursesContainer = document.querySelector('.explore-courses .courses-container');
-let exploreIndex = 0;
-
-function moveExploreSlider(direction) {
-    const courseItems = document.querySelectorAll('.course-item');
-    const totalCourseItems = courseItems.length;
-
-    if (direction === 'prev') {
-        exploreIndex = (exploreIndex === 0) ? totalCourseItems - 4 : exploreIndex - 1;
-    } else {
-        exploreIndex = (exploreIndex === totalCourseItems - 4) ? 0 : exploreIndex + 1;
-    }
-
-    exploreCoursesContainer.style.transform = `translateX(-${exploreIndex * 25}%)`;
-}
-
-if (prevButton && nextButton) {
-    prevButton.addEventListener('click', () => moveSlider('prev'));
-    nextButton.addEventListener('click', () => moveSlider('next'));
-}
-
-if (explorePrevButton && exploreNextButton) {
-    explorePrevButton.addEventListener('click', () => moveExploreSlider('prev'));
-    exploreNextButton.addEventListener('click', () => moveExploreSlider('next'));
-}
-
-
 
